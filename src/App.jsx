b637bd0f4eca5b9d7ede2a25c50d677306ec2f24
“wajahat001” <wajahatchaudy1640@gmail.com>
2024-10-27 22:24:43 +0500

    react-app

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+     <div className="container">
+      <div className="display">
+        5+7 =
+        12
+      </div>
+      <div className="buttons">
+        <ul>
+          <li>+</li>
+          <li>7</li>
+          <li>8</li>
+          <li>9</li>
+          <li>-</li>
+          <li>4</li>
+          <li>5</li>
+          <li>6</li>
+          <li>*</li>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>/</li>
+          <li>0</li>
+          <li>.</li>
+          <li>=</li>
+          <li>C</li>
+        </ul>
+      </div>
+     </div>
+    </>
+  )
+}
+
+export default App
